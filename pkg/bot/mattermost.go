bb97b139810316993b0ce1071367b7b15e5b60a7
Lee Keitel <lee@keitel.xyz>
2018-09-28 22:52:03 -0500

    Move name transforms to team/channel functions

--- pkg/bot/mattermost.go
+++ pkg/bot/mattermost.go
@@ -9,7 +9,6 @@ import (
 func (b *Bot) FindChannelWithTeam(name string) (*model.Channel, error) {
 	c := strings.SplitN(name, ":", 2)
 
-	c[0] = strings.Replace(c[0], " ", "-", -1)
 	team, err := b.FindTeam(c[0])
 	if err != nil {
 		return nil, err
@@ -19,6 +18,7 @@ func (b *Bot) FindChannelWithTeam(name string) (*model.Channel, error) {
 }
 
 func (b *Bot) FindTeam(name string) (*model.Team, error) {
+	name = strings.Replace(name, " ", "-", -1)
 	team, resp := b.c.GetTeamByName(name, "")
 	if resp.Error != nil {
 		return nil, resp.Error
@@ -27,6 +27,7 @@ func (b *Bot) FindTeam(name string) (*model.Team, error) {
 }
 
 func (b *Bot) FindChannel(name, teamID string) (*model.Channel, error) {
+	name = strings.Replace(name, " ", "-", -1)
 	channel, resp := b.c.GetChannelByName(name, teamID, "")
 	if resp.Error != nil {
 		return nil, resp.Error
